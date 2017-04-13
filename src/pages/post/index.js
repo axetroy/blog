8715c732bcbc9602bbfdeffc970c61f2193dc61a
axetroy <troy450409405@gmail.com>
2017-04-13 18:03:01 +0800

    feat: add comments

--- src/pages/post/index.js
+++ src/pages/post/index.js
@@ -9,8 +9,10 @@ import github from '../../lib/github';
 
 import pkg from '../../../package.json';
 
+import './post.css';
+
 class Post extends Component {
-  state = { post: {}, loading: false, number: 0 };
+  state = { post: {}, loading: false, number: 0, comments: [] };
 
   async componentWillMount() {
     let { number } = this.props.match.params;
@@ -48,11 +50,34 @@ class Post extends Component {
         }
       );
       data = res.data;
+      this.getComments(pkg.config.owner, pkg.config.repo, number);
     } catch (err) {
       console.error(err);
     }
     this.setState({ post: data, loading: false });
     return data;
+  }
+
+  async getComments(owner, repo, number) {
+    let comments = [];
+    try {
+      const {
+        data
+      } = await github.get(
+        `/repos/${owner}/${repo}/issues/${number}/comments`,
+        {
+          headers: {
+            Accept: 'application/vnd.github.v3.html'
+          },
+          responseType: 'text'
+        }
+      );
+      console.log(data);
+      this.setState({ comments: data });
+    } catch (err) {
+      console.error(err);
+    }
+    return comments;
   }
 
   render() {
@@ -86,6 +111,41 @@ class Post extends Component {
           style={{ fontSize: '1.6rem', minHeight: '20rem' }}
           dangerouslySetInnerHTML={{ __html: this.state.post.body_html }}
         />
+
+        <hr className="hr" />
+
+        <div>
+          {this.state.comments.map(comment => {
+            return (
+              <div key={comment.id}>
+                <div className="comment-header">
+                  <img
+                    style={{ width: '3.2rem', verticalAlign: 'middle' }}
+                    src={comment.user.avatar_url}
+                    alt=""
+                  />
+                  <strong style={{ color: '#586069' }}>
+                    {comment.user.login}
+                  </strong>
+                  &nbsp;&nbsp;
+                  <span>
+                    {' '}
+                    {
+                      `commented at ${moment(comment.created_at).fromNow()}, updated at ${moment(comment.updated_at).fromNow()}`
+                    }
+                  </span>
+                </div>
+                <div className="comment-body">
+                  <div
+                    className="markdown-body"
+                    style={{ fontSize: '1.6rem', padding: '1.5rem' }}
+                    dangerouslySetInnerHTML={{ __html: comment.body_html }}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </Spin>
     );
   }
