1ada6ec620503db7e6c30f267f3885b9e6125d47
axetroy <troy450409405@gmail.com>
2017-04-14 15:57:30 +0800

    refactor: complete github org information

--- src/component/github-orgs/index.js
+++ src/component/github-orgs/index.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Row, Col, Spin, Popover } from 'antd';
 import sortBy from 'lodash.sortby';
+import Octicon from 'react-octicon';
 import moment from 'moment';
 
 import github from '../../lib/github';
@@ -24,12 +25,18 @@ const styles = {
     left: 0,
     width: '100%',
     height: '100%'
+  },
+  orgRow: {
+    padding: '2rem 0',
+    borderBottom: '0.1rem solid #e6e6e6'
   }
 };
 
 class GithubOrganizations extends Component {
   state = {
-    currentOrg: null
+    currentOrg: null,
+    orgMemberShip: {},
+    orgRepos: {}
   };
 
   async componentWillReceiveProps(nextProp) {}
@@ -37,30 +44,29 @@ class GithubOrganizations extends Component {
   async componentWillMount() {
     // 获取所在的组织列表
     const orgs = await this.getOrganizationsByUser(pkg.config.owner);
-
     this.setState({
       currentOrg: this.props.orgs[0] ? this.props.orgs[0].login : null
     });
-
     // 获取单个组织下的仓库
     for (let org of orgs) {
       await this.getRepositoriesByOrg(org.login);
     }
-
     // 最后进行统计
-    const organizations = { ...{}, ...this.props.orgRepos };
-
+    const organizations = {
+      ...{},
+      ...this.props.orgRepos
+    };
     for (let org in organizations) {
-      const repositories = organizations[org];
-
-      // stat repo one by one
+      const repositories = organizations[org]; // stat repo one by one
       for (let repo of repositories) {
         const stats = await this.statRepo(repo.owner.login, repo.name);
-        this.props.setRepoStat({ name: repo.name, stat: stats });
+        this.props.setRepoStat({
+          name: repo.name,
+          stat: stats
+        });
       }
     }
   }
-
   async getOrganizationsByUser(owner) {
     let organizations = [];
     try {
@@ -69,6 +75,8 @@ class GithubOrganizations extends Component {
       while (_organizations.length) {
         const org = _organizations.shift();
         const { data } = await github.get(`/orgs/${org.login}`); // 获取基本信息
+        await this.getOrgAllMemberShip(org.login); // 获取组织内公开的所有成员
+        await this.getOrgAllRepos(org.login); // 获取该组织下, 所有的项目
         organizations = organizations.concat([data]);
       }
     } catch (err) {
@@ -77,7 +85,6 @@ class GithubOrganizations extends Component {
     this.props.storeOrgs(organizations);
     return organizations;
   }
-
   async getRepositoriesByOrg(org) {
     let repositories = [];
     try {
@@ -86,19 +93,19 @@ class GithubOrganizations extends Component {
     } catch (err) {
       console.error(err);
     }
-    this.props.setOrgRepos({ name: org, repos: repositories });
+    this.props.setOrgRepos({
+      name: org,
+      repos: repositories
+    });
     return repositories;
   }
-
   async statRepo(owner, repo) {
     let contributions = [];
-
     try {
       const response = await github.get(
         `/repos/${owner}/${repo}/stats/contributors`
       );
       let stats = [].concat(response.data || []);
-
       stats.forEach(item => {
         const author = item.author;
         const weeks = item.weeks;
@@ -113,39 +120,144 @@ class GithubOrganizations extends Component {
             contribution.delete += item.d; // delete
             contribution.changes = contribution.add + contribution.delete;
           });
-
-        contributions = []
-          .concat(contributions)
-          .concat([{ author: author, contribution }]);
+        contributions = [].concat(contributions).concat([
+          {
+            author: author,
+            contribution
+          }
+        ]);
       });
     } catch (err) {
       console.error(err);
     }
-
     let total = 0;
-
     contributions.forEach(v => total += v.contribution.changes);
-
     return contributions.map(v => {
       v.contribution.total = total;
       return v;
     });
   }
-
+  async getOrgAllMemberShip(org) {
+    let allMemberShip = [];
+    try {
+      const { data, headers } = await github.get(`/orgs/${org}/public_members`);
+      const { link } = headers;
+      allMemberShip = data;
+      if (link) {
+        // TODO 直接获取最后一页, 可得到组织的总人数
+      }
+    } catch (err) {
+      console.error(err);
+    }
+    this.setState({
+      orgMemberShip: {
+        ...this.state.orgMemberShip,
+        ...{
+          [org]: allMemberShip
+        }
+      }
+    });
+    return allMemberShip;
+  }
+  async getOrgAllRepos(org, page = 1) {
+    let repos = [];
+    try {
+      const { data, headers } = await github.get(`/orgs/${org}/repos`, {
+        params: { page }
+      });
+      repos = data;
+      const { link } = headers;
+      if (link && /rel=['"]next['"]/.test(link)) {
+        return repos.concat(await this.getOrgAllRepos(page + 1));
+      }
+    } catch (err) {
+      console.error(err);
+    }
+    this.setState({
+      orgRepos: {
+        ...this.state.orgRepos,
+        ...{
+          [org]: repos
+        }
+      }
+    });
+    return repos;
+  }
   render() {
     return (
       <Spin spinning={!this.props.orgRepos}>
 
-        <Row>
+        <Row
+          className="text-center"
+          style={{
+            ...styles.orgRow,
+            ...{
+              fontSize: '1.5rem'
+            }
+          }}
+        >
+          <Col
+            span={8}
+            style={{
+              borderRight: '0.1rem solid #e6e6e6'
+            }}
+          >
+            <p>
+              <Octicon className="font-size-2rem mr5" name="mention" mega />
+              {this.state.orgMemberShip[this.state.currentOrg]
+                ? this.state.orgMemberShip[this.state.currentOrg].length
+                : 1}
+            </p>
+            <p>成员数</p>
+          </Col>
+          <Col span={8}>
+            <p>
+              <Octicon className="font-size-2rem mr5" name="repo" mega />
+              {(() => {
+                const currentOrg = this.props.orgs.find(
+                  org => org.login === this.state.currentOrg
+                );
+                return currentOrg ? currentOrg.public_repos : 0;
+              })()}
+            </p>
+            <p>项目数</p>
+          </Col>
+          <Col
+            span={8}
+            style={{
+              borderLeft: '0.1rem solid #e6e6e6'
+            }}
+          >
+            <p>
+              <Octicon className="font-size-2rem mr5" name="star" mega />
+              {(() => {
+                const currentOrg = this.state.orgRepos[this.state.currentOrg];
+                if (currentOrg) {
+                }
+                return currentOrg
+                  ? currentOrg
+                      .map(repo => repo.watchers_count)
+                      .reduce((a, b) => a + b, 0)
+                  : 0;
+              })()}
+            </p>
+            <p>收获star数</p>
+          </Col>
+        </Row>
+
+        <Row style={styles.orgRow}>
           {this.props.orgs.map(v => {
             return (
               <Col
-                onClick={() => this.setState({ currentOrg: v.login })}
+                onClick={() =>
+                  this.setState({
+                    currentOrg: v.login
+                  })}
                 span={6}
                 key={v.login}
                 style={{
                   textAlign: 'center',
-                  borderBottomWidth: '0.5rem',
+                  borderBottomWidth: '0.3rem',
                   borderBottomStyle: 'solid',
                   borderBottomColor: this.state.currentOrg === v.login
                     ? '#008000'
@@ -154,7 +266,10 @@ class GithubOrganizations extends Component {
               >
                 <div>
                   <img
-                    style={{ width: '10rem', maxWidth: '100%' }}
+                    style={{
+                      width: '10rem',
+                      maxWidth: '100%'
+                    }}
                     src={v.avatar_url}
                     alt=""
                   />
@@ -165,9 +280,7 @@ class GithubOrganizations extends Component {
           })}
         </Row>
 
-        <hr className="hr" />
-
-        <Row>
+        <Row style={styles.orgRow}>
           <div>
             {this.props.orgs
               .filter(org => org.login === this.state.currentOrg)
@@ -185,9 +298,7 @@ class GithubOrganizations extends Component {
           </div>
         </Row>
 
-        <hr className="hr" />
-
-        <Row>
+        <Row style={styles.orgRow}>
           {sortBy(
             this.props.orgRepos[this.state.currentOrg] || [],
             repo => -repo.watchers_count
@@ -211,9 +322,11 @@ class GithubOrganizations extends Component {
                             stat.author.login === pkg.config.owner
                         );
                         if (myStat) {
-                          return (myStat.contribution.changes /
-                            myStat.contribution.total *
-                            100).toFixed(2) + '%';
+                          return (
+                            (myStat.contribution.changes /
+                              myStat.contribution.total *
+                              100).toFixed(2) + '%'
+                          );
                         } else {
                           return '0%';
                         }
@@ -253,9 +366,11 @@ class GithubOrganizations extends Component {
                               stat.author.login === pkg.config.owner
                           );
                           if (myStat) {
-                            return (myStat.contribution.changes /
-                              myStat.contribution.total *
-                              100).toFixed(2) + '%';
+                            return (
+                              (myStat.contribution.changes /
+                                myStat.contribution.total *
+                                100).toFixed(2) + '%'
+                            );
                           } else {
                             return '0%';
                           }
@@ -265,7 +380,12 @@ class GithubOrganizations extends Component {
                     />
                   </div>
 
-                  <span style={{ position: 'absolute', color: '#fff' }}>
+                  <span
+                    style={{
+                      position: 'absolute',
+                      color: '#fff'
+                    }}
+                  >
                     <span> {repo.name} </span>
                   </span>
 
@@ -281,7 +401,6 @@ class GithubOrganizations extends Component {
     );
   }
 }
-
 export default connect(
   function mapStateToProps(state) {
     return {
@@ -292,11 +411,7 @@ export default connect(
   },
   function mapDispatchToProps(dispatch) {
     return bindActionCreators(
-      {
-        storeOrgs: store,
-        setOrgRepos: set,
-        setRepoStat: setStat
-      },
+      { storeOrgs: store, setOrgRepos: set, setRepoStat: setStat },
       dispatch
     );
   }
