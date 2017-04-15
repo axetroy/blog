45717ae26c81cc1047d60eda4ec97c3a846fa1b9
axetroy <troy450409405@gmail.com>
2017-04-15 21:16:18 +0800

    update

--- src/component/github-repo/index.js
+++ src/component/github-repo/index.js
@@ -41,16 +41,6 @@ class GithubRepositories extends Component {
   }
 
   render() {
-    const data = {
-      labels: ['Red', 'Blue', 'Yellow'],
-      datasets: [
-        {
-          data: [300, 50, 100],
-          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
-        }
-      ]
-    };
     return (
       <Spin spinning={false}>
         <Row
