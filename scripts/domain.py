bc4713a14457511c6b1463fc57769a9cd295791a
axetroy <troy450409405@gmail.com>
2017-04-17 11:15:17 +0800

    feat: binding the domain

---
+++ scripts/domain.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+cwd = os.getcwd()
+
+pkgPath = os.path.join(cwd, 'package.json')
+
+with open(pkgPath, 'r') as f:
+    str = f.read()
+    pkg = json.loads(str)
+
+    config = pkg["config"]
+
+    domain = config["domain"]
+
+    if domain:
+        with open(os.path.join(cwd, 'build', 'CNAME'), 'w') as cname:
+            cname.write(domain)
