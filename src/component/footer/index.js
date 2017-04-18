5a1323d1697bf6cb28686d114c310fcb10b74dcf
axetroy <troy450409405@gmail.com>
2017-04-18 17:29:12 +0800

    update

--- src/component/footer/index.js
+++ src/component/footer/index.js
@@ -7,7 +7,7 @@ import moment from 'moment';
 
 class Footer extends Component {
   state = {
-    createdDate: new Date('2016-11-09 14:22:33'),
+    created: moment(new Date('2016-11-09 14:22:33')),
     days: 0,
     hours: 0,
     minutes: 0,
@@ -24,9 +24,7 @@ class Footer extends Component {
   }
 
   timer() {
-    const created = moment(this.state.createdDate);
-
-    let seconds = moment().diff(created, 'seconds');
+    let seconds = moment().diff(this.state.created, 'seconds');
     const days = Math.floor(seconds / (3600 * 24));
     seconds = seconds - days * 3600 * 24;
 
@@ -48,13 +46,13 @@ class Footer extends Component {
   render() {
     return (
       <Row className="text-center">
-        <Col md={8} xs={0}>
+        <Col md={8} sm={12} xs={0}>
           <h3><Icon type="github" />Github</h3>
           <div>
             <a target="_blank" href="https://github.com/axetroy/blog">仓库源码</a>
           </div>
         </Col>
-        <Col md={8} xs={0}>
+        <Col md={8} sm={12} xs={0}>
           <h3><Icon type="link" />相关技术</h3>
           <div>
             <a target="_blank" href="https://facebook.github.io/react/">
@@ -66,7 +64,7 @@ class Footer extends Component {
           </div>
           <div><a target="_blank" href="https://ant.design">Antd</a></div>
         </Col>
-        <Col md={8} xs={24}>
+        <Col md={8} sm={0} xs={24}>
           <p>Copyright © 2017</p>
           <p>
             {`博客已运行 ${this.state.days}d ${this.state.hours}h ${this.state.minutes}m ${this.state.seconds}s`}
