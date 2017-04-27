2023b245a2bd451030fd8d2a5960de2d6bd85023
axetroy <troy450409405@gmail.com>
2017-04-27 10:08:37 +0800

    Revert "refactor: 尝试添加登陆后评论"
    This reverts commit 3e255bacdda9197c2a6fddc23188781d574bfff3.

--- src/pages/post/index.js
+++ src/pages/post/index.js
@@ -4,13 +4,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Spin, Tag, Tooltip, Icon, Input, Tabs, Button, Modal } from 'antd';
+import { Spin, Tag, Tooltip, Icon } from 'antd';
 import moment from 'moment';
 
-const TabPane = Tabs.TabPane;
 import github from '../../lib/github';
 import * as postAction from '../../redux/post';
-import * as markdownPreviewAction from '../../redux/tool-md-preview';
 import pkg from '../../../package.json';
 
 import './post.css';
@@ -76,60 +74,12 @@ class Post extends Component {
     return comments;
   }
 
-  async onTabChange(tab) {
-    let html = '';
-    if (tab === 'preview') {
-      const { data } = await github.post(
-        '/markdown',
-        {
-          text: this.props.TOOL_MD_PREVIEW.markdown,
-          mode: 'markdown'
-        },
-        {
-          responseType: 'text'
-        }
-      );
-      this.props.storeHTML(data);
-    }
-    return html;
-  }
-
-  async submit(number) {
-    const { owner, repo } = pkg.config;
-    try {
-      await github.post(
-        `/repos/${owner}/${repo}/issues/${number}/comments`,
-        {
-          body: this.props.TOOL_MD_PREVIEW.markdown
-        },
-        {
-          params: {
-            access_token: this.props.OAUTH.access_token
-          }
-        }
-      );
-      await this.getComments(owner, repo, number);
-      this.props.storeMd('');
-      this.props.storeHTML('');
-    } catch (err) {
-      console.error(err);
-      Modal.error({
-        title: '发生错误',
-        content: err + ''
-      });
-    }
-  }
   render() {
     const { number } = this.props.match.params;
     const post = this.props.POST[number] || {};
     return (
       <Spin spinning={!Object.keys(post).length}>
-        <div
-          className="edit-this-page-container"
-          style={{
-            padding: '2.4rem'
-          }}
-        >
+        <div className="edit-this-page-container" style={{ padding: '2.4rem' }}>
           <h2>{post.title}</h2>
           <div>
             {post.user
@@ -145,11 +95,7 @@ class Post extends Component {
               : ''}
 
           </div>
-          <div
-            style={{
-              margin: '1rem 0'
-            }}
-          >
+          <div style={{ margin: '1rem 0' }}>
             {post.labels &&
               post.labels.map((v, i) => {
                 return (
@@ -186,7 +132,20 @@ class Post extends Component {
           />
           <hr className="hr" />
           <div>
-            <h3>留言: </h3>
+            <h3>大牛们的评论: </h3>
+
+            <p
+              style={{
+                fontSize: '1.5rem'
+              }}
+            >
+              <a
+                target="_blank"
+                href={`https://github.com/${pkg.config.owner}/${pkg.config.repo}/issues/${post.number}`}
+              >
+                朕有话说
+              </a>
+            </p>
 
             {this.state.comments.length
               ? this.state.comments.map(comment => {
@@ -235,12 +194,7 @@ class Post extends Component {
                           {`updated at ${moment(comment.updated_at).fromNow()}`}
                         </span>
                       </div>
-                      <div
-                        className="comment-body"
-                        style={{
-                          minHeight: '10rem'
-                        }}
-                      >
+                      <div className="comment-body">
                         <div
                           className="markdown-body"
                           style={{
@@ -259,70 +213,9 @@ class Post extends Component {
                   <p>还没有人评论哦，赶紧抢沙发!</p>
                 </div>}
 
-            <h3>
-              <a
-                target="_blank"
-                href={`https://github.com/${pkg.config.owner}/${pkg.config.repo}/issues/${post.number}`}
-              >
-                我要发表看法
-              </a>
-            </h3>
-
-            {this.props.USER && this.props.USER.name
-              ? <div>
-                  <Tabs
-                    defaultActiveKey="source"
-                    onChange={tab => this.onTabChange(tab)}
-                    animated={false}
-                  >
-                    <TabPane tab="Markdown" key="source">
-                      <Input
-                        defaultValue={this.props.TOOL_MD_PREVIEW.markdown}
-                        style={{
-                          fontSize: '1.6rem'
-                        }}
-                        type="textarea"
-                        placeholder="以Markdown格式书写"
-                        autosize={{
-                          minRows: 10,
-                          maxRows: 20
-                        }}
-                        onChange={event =>
-                          this.props.storeMd(event.target.value)}
-                      />
-                    </TabPane>
-                    <TabPane tab="Preview" key="preview">
-                      <div
-                        className="markdown-body"
-                        style={{
-                          fontSize: '1.6rem',
-                          minHeight: '20rem'
-                        }}
-                        dangerouslySetInnerHTML={{
-                          __html: this.props.TOOL_MD_PREVIEW.html + ''
-                        }}
-                      />
-                    </TabPane>
-                  </Tabs>
-
-                  <div
-                    style={{
-                      margin: '2rem 0'
-                    }}
-                  >
-                    <Button
-                      type="primary"
-                      size="large"
-                      onClick={() => this.submit(post.number)}
-                    >
-                      Submit
-                    </Button>
-                  </div>
-                </div>
-              : ''}
-
           </div>
         </div>
+
       </Spin>
     );
   }
@@ -330,18 +223,13 @@ class Post extends Component {
 export default connect(
   function mapStateToProps(state) {
     return {
-      POST: state.POST,
-      TOOL_MD_PREVIEW: state.TOOL_MD_PREVIEW,
-      OAUTH: state.OAUTH,
-      USER: state.USER
+      POST: state.POST
     };
   },
   function mapDispatchToProps(dispatch) {
     return bindActionCreators(
       {
-        setPost: postAction.set,
-        storeHTML: markdownPreviewAction.setHTML,
-        storeMd: markdownPreviewAction.setMarkdown
+        setPost: postAction.set
       },
       dispatch
     );
