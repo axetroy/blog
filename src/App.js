b2c48a9512fc8ba54fe4d6f5a5d3f82897cffd32
axetroy <troy450409405@gmail.com>
2017-05-04 03:19:53 +0800

    feat: add click animation

--- src/App.js
+++ src/App.js
@@ -23,6 +23,8 @@ import Gists from './pages/gists';
 import Gist from './pages/gist';
 import Footer from './component/footer';
 import Header from './component/header';
+import ClickMaterial from './component/click-material';
+
 import store from './redux/index';
 
 import './App.css';
@@ -34,82 +36,84 @@ class App extends Component {
     return (
       <Provider store={store}>
         <Router history={HashRouter}>
-          <Row>
-            <Col
-              lg={{ span: 16, offset: 4 }}
-              md={{ span: 18, offset: 3 }}
-              sm={{ span: 20, offset: 2 }}
-              xs={{ span: 24 }}
-            >
-              <Header />
-              <a
-                target="_blank"
-                href={`https://github.com/${pkg.config.owner}/${pkg.config.repo}`}
+          <ClickMaterial>
+            <Row>
+              <Col
+                lg={{ span: 16, offset: 4 }}
+                md={{ span: 18, offset: 3 }}
+                sm={{ span: 20, offset: 2 }}
+                xs={{ span: 24 }}
               >
-                <svg
-                  width="80"
-                  height="80"
-                  viewBox="0 0 250 250"
+                <Header />
+                <a
+                  target="_blank"
+                  href={`https://github.com/${pkg.config.owner}/${pkg.config.repo}`}
+                >
+                  <svg
+                    width="80"
+                    height="80"
+                    viewBox="0 0 250 250"
+                    style={{
+                      fill: '#64CEAA',
+                      color: '#fff',
+                      position: 'absolute',
+                      top: '0',
+                      border: 0,
+                      right: 0
+                    }}
+                  >
+                    <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
+                    <path
+                      d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
+                      fill="currentColor"
+                      style={{ transformOrigin: '130px 106px' }}
+                      className="octo-arm"
+                    />
+                    <path
+                      d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z"
+                      fill="currentColor"
+                      className="octo-body"
+                    />
+                  </svg>
+                </a>
+              </Col>
+              <Col
+                lg={{ span: 16, offset: 4 }}
+                md={{ span: 18, offset: 3 }}
+                sm={{ span: 20, offset: 2 }}
+                xs={{ span: 24 }}
+              >
+                <Card
                   style={{
-                    fill: '#64CEAA',
-                    color: '#fff',
-                    position: 'absolute',
-                    top: '0',
-                    border: 0,
-                    right: 0
+                    marginTop: '2rem'
                   }}
                 >
-                  <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
-                  <path
-                    d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
-                    fill="currentColor"
-                    style={{ transformOrigin: '130px 106px' }}
-                    className="octo-arm"
-                  />
-                  <path
-                    d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z"
-                    fill="currentColor"
-                    className="octo-body"
-                  />
-                </svg>
-              </a>
-            </Col>
-            <Col
-              lg={{ span: 16, offset: 4 }}
-              md={{ span: 18, offset: 3 }}
-              sm={{ span: 20, offset: 2 }}
-              xs={{ span: 24 }}
-            >
-              <Card
-                style={{
-                  marginTop: '2rem'
-                }}
+                  <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/github" component={Github} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/tool" component={Tool} />
+                    <Route exact path="/post/:number" component={Post} />
+                    <Route exact path="/post" component={Posts} />
+                    <Route exact path="/repo/:repo" component={Repo} />
+                    <Route exact path="/repo" component={Repos} />
+                    <Route exact path="/todo/:number" component={Todo} />
+                    <Route exact path="/todo" component={Todos} />
+                    <Route exact path="/gist/:id" component={Gist} />
+                    <Route exact path="/gist" component={Gists} />
+                  </Switch>
+                </Card>
+              </Col>
+              <Col
+                lg={{ span: 16, offset: 4 }}
+                md={{ span: 18, offset: 3 }}
+                sm={{ span: 20, offset: 2 }}
+                xs={{ span: 24 }}
               >
-                <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route exact path="/github" component={Github} />
-                  <Route exact path="/about" component={About} />
-                  <Route exact path="/tool" component={Tool} />
-                  <Route exact path="/post/:number" component={Post} />
-                  <Route exact path="/post" component={Posts} />
-                  <Route exact path="/repo/:repo" component={Repo} />
-                  <Route exact path="/repo" component={Repos} />
-                  <Route exact path="/todo/:number" component={Todo} />
-                  <Route exact path="/todo" component={Todos} />
-                  <Route exact path="/gist/:id" component={Gist} />
-                  <Route exact path="/gist" component={Gists} />
-                </Switch>
-              </Card>
-            </Col>
-            <Col
-              lg={{ span: 16, offset: 4 }}
-              md={{ span: 18, offset: 3 }}
-              sm={{ span: 20, offset: 2 }}
-              xs={{ span: 24 }}
-            >
-              <Footer />
-            </Col>
-          </Row>
+                <Footer />
+              </Col>
+            </Row>
+          </ClickMaterial>
         </Router>
       </Provider>
     );
---
+++ src/component/click-material/index.js
@@ -0,0 +1,51 @@
+/**
+ * Created by axetroy on 17-4-6.
+ */
+import React, { Component } from 'react';
+
+import './index.css';
+
+class Click extends Component {
+  componentWillMount() {
+    const width = 200;
+    const height = 200;
+    const ele = document.createElement('span');
+    ele.classList.add('ripple');
+    ele.style.width = `${width}px`;
+    ele.style.height = `${height}px`;
+    this.__ele__ = ele;
+  }
+  componentWillUnmount() {
+    (this.__timer__ || []).forEach(timerId => {
+      clearTimeout(timerId);
+    });
+  }
+  onClick(event) {
+    const { pageX, pageY } = event;
+    const width = 200;
+    const height = 200;
+
+    if (!this.__ele__) return;
+
+    const ele = this.__ele__.cloneNode(true);
+    ele.style.left = `${pageX - width / 2}px`;
+    ele.style.top = `${pageY - height / 2}px`;
+    document.body.appendChild(ele);
+
+    this.__timer__ = this.__timer__ || [];
+    this.__timer__ = this.__timer__.concat([
+      setTimeout(() => {
+        ele.remove();
+      }, 750)
+    ]);
+  }
+
+  render() {
+    return (
+      <div onClick={this.onClick.bind(this)}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+export default Click;
