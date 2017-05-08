4b8c0f475be84014bf5220a2f9442dfd5fc3dcbb
axetroy <troy450409405@gmail.com>
2017-05-08 15:28:34 +0800

    refactor: apply @axetroy/react-now component

--- src/component/footer/index.js
+++ src/component/footer/index.js
@@ -3,34 +3,14 @@
  */
 import React, { Component } from 'react';
 import { Row, Col } from 'antd';
+import Now from '@axetroy/react-now';
 
 import { diffTime } from '../../lib/utils';
 
 class Footer extends Component {
   state = {
-    created: new Date('2016-11-09 14:22:33'),
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0
+    created: new Date('2016-11-09 14:22:33')
   };
-
-  componentDidMount() {
-    const intervalId = setInterval(this.timer.bind(this), 1000);
-    this.setState({ intervalId: intervalId });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.state.intervalId);
-  }
-
-  timer() {
-    const diff = diffTime(new Date('2016-11-09 14:22:33'))(new Date());
-    this.setState({
-      date: new Date(),
-      ...diff
-    });
-  }
 
   render() {
     return (
@@ -44,9 +24,21 @@ class Footer extends Component {
       >
         <Col span={24}>
           <p>Copyright © 2017</p>
-          <p>
-            {`已运行 ${this.state.days}天 ${this.state.hours}时 ${this.state.minutes}分 ${this.state.seconds}秒`}
-          </p>
+          <Now>
+            {now => {
+              const diff = diffTime(this.state.created)(now);
+              return (
+                <p>
+                  {`已运行
+                  ${diff.days}天
+                  ${diff.hours}时
+                  ${diff.minutes}分
+                  ${diff.seconds}秒
+                  `}
+                </p>
+              );
+            }}
+          </Now>
           <p>
             Created by
             {' '}
