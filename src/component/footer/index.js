fd711cfe6322c369e66d4261e3756bdf99933e77
axetroy <troy450409405@gmail.com>
2017-06-10 00:51:32 +0800

    feat(footer): 添加统计和firebase链接

--- src/component/footer/index.js
+++ src/component/footer/index.js
@@ -66,9 +66,25 @@ class Footer extends Component {
             }}
           </Now>
           <p>
-            Created by
-            {' '}
+            Created by{' '}
             <a target="_blank" href="https://github.com/axetroy">Axetroy</a>
+          </p>
+          <p>
+            <a
+              href="https://console.firebase.google.com/project/blog-9281a/database/data"
+              target="_blank"
+              rel="nofollow"
+            >
+              Firebase
+            </a>
+            &nbsp;&nbsp;
+            <a
+              href="https://analytics.google.com/analytics/web/?hl=zh-CN&pli=1#report/defaultid/a98287100w144548599p149233935/"
+              target="_blank"
+              rel="nofollow"
+            >
+              站长统计
+            </a>
           </p>
         </Col>
       </Row>
