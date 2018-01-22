05a71c4bdb9ebfcbe887e0430c989f935b94e54d
axetroy <troy450409405@gmail.com>
2018-01-22 17:59:58 +0800

    feat: init with dva

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 // @flow
 // polyfill
-import FastClick from 'fastclick';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-import { onStoreDone } from './redux/createStore';
-import { init } from './lib/firebase';
+import FastClick from "fastclick";
+import React from "react";
+import ReactDOM from "react-dom";
+import dva from "dva";
+import { Router, Route } from "dva/router";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+import { onStoreDone } from "./redux/createStore";
+import { init } from "./lib/firebase";
 
 FastClick.attach(document.body);
 
@@ -15,10 +17,14 @@ FastClick.attach(document.body);
 onStoreDone(function() {
   // wait firebase login done
   init(function() {});
-  ReactDOM.render(
-    <App style={{ height: '100%' }} />,
-    document.getElementById('root')
-  );
+  // ReactDOM.render(
+  //   <App style={{ height: '100%' }} />,
+  //   document.getElementById('root')
+  // );
+  const app = dva();
+  console.log(123, app._store);
+  app.router(({ history }) => <App style={{ height: "100%" }} />);
+  app.start("#root");
 });
 
 registerServiceWorker();
