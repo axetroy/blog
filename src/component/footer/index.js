1bc615f529b784d1f056fa32a1cefdbe3b72102c
axetroy <troy450409405@gmail.com>
2019-03-29 23:19:45 +0800

    refactor: footer

--- src/component/footer/index.js
+++ src/component/footer/index.js
@@ -4,6 +4,7 @@
 import React, { Component } from "react";
 import Now from "@axetroy/react-now";
 import moment from "moment";
+import { Row, Col } from "antd";
 
 import { diffTime } from "../../lib/utils";
 import "./index.css";
@@ -16,49 +17,164 @@ class Footer extends Component {
   render() {
     const LAST_UPDATE_TIME = new Date(+process.env.REACT_APP_PUBLISH_DATE);
     return (
-      <div id="footer">
-        <p>Copyright © 2017</p>
-        <Now>
-          {now => {
-            const diff = diffTime(this.state.created)(now);
-            return (
-              <div>
-                <p>
-                  {`已运行
+      <footer>
+        <Row id="footer">
+          <Col
+            xs={{ span: 11, offset: 1 }}
+            sm={{ span: 5, offset: 1 }}
+            md={{ span: 4, offset: 4 }}
+            lg={{ span: 4, offset: 4 }}
+            xl={{ span: 4, offset: 4 }}
+            xxl={{ span: 4, offset: 4 }}
+          >
+            <p className="footer-item-name">相关链接</p>
+            <ul>
+              <li>
+                <a
+                  href="https://github.com/axetroy"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                >
+                  Github
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/axetroy/blog"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                >
+                  网站源码
+                </a>
+              </li>
+            </ul>
+          </Col>
+          <Col
+            xs={{ span: 11 }}
+            sm={{ span: 5 }}
+            md={{ span: 4 }}
+            lg={{ span: 4 }}
+            xl={{ span: 4 }}
+            xxl={{ span: 4 }}
+          >
+            <p className="footer-item-name">免费的API服务</p>
+            <ul>
+              <li>
+                <a
+                  href="https://github.com/axetroy/ip"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                >
+                  获取当前IP
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/axetroy/locate"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                >
+                  根据IP获取定位信息
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/axetroy/proxy"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                >
+                  HTTP请求代理
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/axetroy/email"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                >
+                  发送邮件
+                </a>
+              </li>
+            </ul>
+          </Col>
+          <Col
+            xs={{ span: 11, offset: 1 }}
+            sm={{ span: 5 }}
+            md={{ span: 4 }}
+            lg={{ span: 4 }}
+            xl={{ span: 4 }}
+            xxl={{ span: 4 }}
+          >
+            <p className="footer-item-name">友情链接</p>
+            <ul>
+              <li>
+                <a
+                  href="javascript: void 0;"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                >
+                  留言加个友链可好?
+                </a>
+              </li>
+            </ul>
+          </Col>
+          <Col
+            xs={{ span: 11 }}
+            sm={{ span: 5 }}
+            md={{ span: 4 }}
+            lg={{ span: 4 }}
+            xl={{ span: 4 }}
+            xxl={{ span: 4 }}
+          >
+            <p className="footer-item-name">关于本站</p>
+            <ul>
+              <li>Copyright © {new Date().getFullYear()}</li>
+              <li>
+                <Now>
+                  {now => {
+                    const diff = diffTime(this.state.created)(now);
+                    return (
+                      <div>
+                        <p>
+                          {`已运行
                   ${diff.days}天
                   ${diff.hours}时
                   ${diff.minutes}分
                   ${diff.seconds}秒
                   `}
-                </p>
-                <p>
-                  最近更新&nbsp;
-                  {moment(LAST_UPDATE_TIME).fromNow()}
-                </p>
-              </div>
-            );
-          }}
-        </Now>
-        <p>
-          Created by{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/axetroy"
-          >
-            Axetroy
-          </a>
-        </p>
-        <p>
-          <a
-            href="https://analytics.google.com/analytics/web/?hl=zh-CN&pli=1#report/defaultid/a98287100w144548599p149233935/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            站长统计
-          </a>
-        </p>
-      </div>
+                        </p>
+                        <p>
+                          最近更新&nbsp;
+                          {moment(LAST_UPDATE_TIME).fromNow()}
+                        </p>
+                      </div>
+                    );
+                  }}
+                </Now>
+              </li>
+              <li>
+                Created by{" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/axetroy"
+                >
+                  Axetroy
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://analytics.google.com/analytics/web/?hl=zh-CN&pli=1#report/defaultid/a98287100w144548599p149233935/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  站长统计
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </footer>
     );
   }
 }
